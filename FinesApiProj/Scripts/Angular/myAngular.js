cbf635017913425a616cf570d57934b30d450ffb
Fawad <fawad_12@outlook.com>
2019-12-22 23:13:11 +0500

    ng-controller and ng-repeat

--- FinesApiProj/Scripts/Angular/myAngular.js
+++ FinesApiProj/Scripts/Angular/myAngular.js
@@ -1,1 +1,9 @@
 ﻿var app = angular.module("Homeapp", []);
+app.controller("MyFirstController", function ($scope) {
+    $scope.color = "pink";
+    $scope.countries = ['Pakistan', 'India', 'China', 'Afghanistan'];
+    $scope.calculateWidth = function () {
+        width = parseInt($scope.hw) + (parseInt($scope.hw)/2);
+        return width;
+    };
+});
