1f8767d324b87b6d8fe8a38aa1cf130ecc569a0f
Fawad <fawad_12@outlook.com>
2019-12-22 23:49:17 +0500

    using filters

--- FinesApiProj/Scripts/Angular/myAngular.js
+++ FinesApiProj/Scripts/Angular/myAngular.js
@@ -1,24 +1,10 @@
 ﻿var app = angular.module("Homeapp", []);
 app.controller("MyFirstController", function ($scope) {
-    $scope.color = "pink";
-    $scope.countries = ['Pakistan', 'India', 'China', 'Afghanistan'];
-    $scope.calculateWidth = function () {
-        width = parseInt($scope.hw) + (parseInt($scope.hw)/2);
-        return width;
-    };
-});
-
-app.run(function ($rootScope) {
-    $rootScope.msg = "Hello, I am root Scope, I can be accessed outside controller too";
-});
-app.directive("myCustomColorList", function () {
-    var content = "<select ng-model='color'>";
-    content += "<option>red</option>";
-    content += "<option>blue</option>";
-    content += "<option>yellow</option>";
-    content += "<option>green</option>";
-    content += "</select>";
-    return {
-        template: content
-    };
+    $scope.students = [
+        {"name":"Ali","reg":"bcs123456"},
+        {"name":"Zohaib","reg":"bcs191001"},
+        {"name":"Bilal","reg":"bcs183456"},
+        {"name":"Mubashir","reg":"bce161456"},
+        {"name":"Hamdan","reg":"bse143001"}
+    ];
 });
