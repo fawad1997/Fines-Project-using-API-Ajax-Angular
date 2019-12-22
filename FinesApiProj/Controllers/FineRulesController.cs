a8f9d258cbb72c33fe5b5bcf773e90d5e8bbbdf7
Fawad <fawad_12@outlook.com>
2019-12-22 17:06:16 +0500

    Fine Rules and Fines API Created

---
+++ FinesApiProj/Controllers/FineRulesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FinesApiProj.Models;
+
+namespace FinesApiProj.Controllers
+{
+    public class FineRulesController : ApiController
+    {
+        private FinesDbEntities db = new FinesDbEntities();
+
+        // GET: api/FineRules
+        public IQueryable<FineRules> GetFineRules()
+        {
+            return db.FineRules;
+        }
+
+        // GET: api/FineRules/5
+        [ResponseType(typeof(FineRules))]
+        public IHttpActionResult GetFineRules(int id)
+        {
+            FineRules fineRules = db.FineRules.Find(id);
+            if (fineRules == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(fineRules);
+        }
+
+        // PUT: api/FineRules/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutFineRules(int id, FineRules fineRules)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != fineRules.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(fineRules).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FineRulesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/FineRules
+        [ResponseType(typeof(FineRules))]
+        public IHttpActionResult PostFineRules(FineRules fineRules)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.FineRules.Add(fineRules);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = fineRules.Id }, fineRules);
+        }
+
+        // DELETE: api/FineRules/5
+        [ResponseType(typeof(FineRules))]
+        public IHttpActionResult DeleteFineRules(int id)
+        {
+            FineRules fineRules = db.FineRules.Find(id);
+            if (fineRules == null)
+            {
+                return NotFound();
+            }
+
+            db.FineRules.Remove(fineRules);
+            db.SaveChanges();
+
+            return Ok(fineRules);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FineRulesExists(int id)
+        {
+            return db.FineRules.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ FinesApiProj/Controllers/FinesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FinesApiProj.Models;
+
+namespace FinesApiProj.Controllers
+{
+    public class FinesController : ApiController
+    {
+        private FinesDbEntities db = new FinesDbEntities();
+
+        // GET: api/Fines
+        public IQueryable<Fines> GetFines()
+        {
+            return db.Fines;
+        }
+
+        // GET: api/Fines/5
+        [ResponseType(typeof(Fines))]
+        public IHttpActionResult GetFines(int id)
+        {
+            Fines fines = db.Fines.Find(id);
+            if (fines == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(fines);
+        }
+
+        // PUT: api/Fines/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutFines(int id, Fines fines)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != fines.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(fines).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FinesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Fines
+        [ResponseType(typeof(Fines))]
+        public IHttpActionResult PostFines(Fines fines)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Fines.Add(fines);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = fines.Id }, fines);
+        }
+
+        // DELETE: api/Fines/5
+        [ResponseType(typeof(Fines))]
+        public IHttpActionResult DeleteFines(int id)
+        {
+            Fines fines = db.Fines.Find(id);
+            if (fines == null)
+            {
+                return NotFound();
+            }
+
+            db.Fines.Remove(fines);
+            db.SaveChanges();
+
+            return Ok(fines);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FinesExists(int id)
+        {
+            return db.Fines.Count(e => e.Id == id) > 0;
+        }
+    }
+}
