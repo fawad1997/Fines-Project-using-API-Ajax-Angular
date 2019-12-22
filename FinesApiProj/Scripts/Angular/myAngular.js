fc78b05b870c59e6e8a9d4b76604bc6a5d4f58c9
Fawad <fawad_12@outlook.com>
2019-12-23 00:04:40 +0500

    using timeout service

--- FinesApiProj/Scripts/Angular/myAngular.js
+++ FinesApiProj/Scripts/Angular/myAngular.js
@@ -1,7 +1,12 @@
 ﻿var app = angular.module("Homeapp", []);
-app.controller("MyFirstController", function ($scope, $location) {
+app.controller("MyFirstController", function ($scope, $location,$timeout) {
     $scope.url = $location.absUrl();
     $scope.protocol = $location.protocol();
     $scope.host = $location.host();
     $scope.port = $location.port();
+    $scope.myfunc = function () {
+        $timeout(function () {
+            $scope.msg = "This msg will be displayed after 2 seconds";
+        },2000);
+    };
 });
